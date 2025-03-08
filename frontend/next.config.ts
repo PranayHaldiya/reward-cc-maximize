5315ecf844d16ff4c2a2adc3a71b73bbbeb630fb
Pranay Haldiya <pranayhaldiya8127@gmail.com>
2025-03-08 18:47:53 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,11 @@
 const nextConfig = {
   images: {
     domains: ['localhost', 'your-production-domain.com'],
+    unoptimized: process.env.NODE_ENV === 'development',
   },
   eslint: {
-    dirs: ['pages', 'utils', ], // Include app directory for ESLint
+    dirs: [ 'utils'],
+    ignoreDuringBuilds: false,
   },
   typescript: {
     ignoreBuildErrors: false,
