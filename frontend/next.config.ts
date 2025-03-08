f532592d7b9fa99b39fdf5c786354efc7fd549ee
Pranay Haldiya <pranayhaldiya8127@gmail.com>
2025-03-08 13:49:00 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'your-production-domain.com'],
+     eslint: {
+      ignoreDuringBuilds: false,
   },
 };
 
